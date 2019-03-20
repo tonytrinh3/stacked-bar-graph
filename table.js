ed1fbb5e08740bf26b7df20c16eb790f170d75ef
Tony <tony.w.trinh3@gmail.com>
2019-03-19 19:11:50 -0700

    Add files via upload
    -changed table

--- table.js
+++ table.js
@@ -1,86 +1,96 @@
 //WSEC Standard Design LPD (W/sf)
 const WSECDesign = [
-    {spaceType: "Conference/ Meeting/ Multipurpose", wattSF: 0.98, alt: 0.60 },
-    {spaceType: "Convention center - Exhibit space", wattSF: 1.16, alt: 0.60},
-    {spaceType: "Corridor/ Transition", wattSF: 0.53, alt: 0.60},
-    {spaceType: "Dining area", wattSF: 0.86, alt: 0.60},
-    {spaceType: "Electrical/ Mechanical", wattSF: 0.76, alt: 0.60},
-    {spaceType: "Food preparation", wattSF: 0.79, alt: 0.60},
-    {spaceType: "Lobby", wattSF: 0.60, alt: 0.60},
-    {spaceType: "Restrooms", wattSF: 0.78, alt: 0.60},
-    {spaceType: "Stairs – Active", wattSF: 0.55, alt: 0.60}
+    {label: "Conference/ Meeting/ Multipurpose", value: 0.98, alt: 0.60 },
+    {label: "Convention center - Exhibit space", value: 1.16, alt: 0.60},
+    {label: "Corridor/ Transition", value: 0.53, alt: 0.60},
+    {label: "Dining area", value: 0.86, alt: 0.60},
+    {label: "Electrical/ Mechanical", value: 0.76, alt: 0.60},
+    {label: "Food preparation", value: 0.79, alt: 0.60},
+    {label: "Lobby", value: 0.60, alt: 0.60},
+    {label: "Restrooms", value: 0.78, alt: 0.60},
+    {label: "Stairs - Active", value: 0.55, alt: 0.60}
 ];
+
+
+const columns = [['Space Type', "WSEC Standard Design \n LPD (W/sf)", "Option 2 Design \n LPD (W/sf)"],
+['Envelope', "Washington Code", "Proposed Design"]]
+
+const selector = ['label', 'value', "alt"]
+/*
 //Option 2 Design LPD (W/sf)
-
-const columns = ['Space Type', "WSEC Standard Design \n LPD (W/sf)", "Option 2 Design \n LPD (W/sf)"]
-const selector = ['spaceType', 'wattSF', "alt"]
-
 const option2Design = [
-    {spaceType: "Conference/ Meeting/ Multipurpose", wattSF: 0.60},
-    {spaceType: "Convention center - Exhibit space", wattSF: 0.60},
-    {spaceType: "Corridor/ Transition", wattSF: 0.60},
-    {spaceType: "Dining area", wattSF: 0.60},
-    {spaceType: "Electrical/ Mechanical", wattSF: 0.60},
-    {spaceType: "Food preparation", wattSF: 0.60},
-    {spaceType: "Lobby", wattSF: 0.60},
-    {spaceType: "Restrooms", wattSF: 0.60},
-    {spaceType: "Stairs – Active", wattSF: 0.60}
+    {spaceType: "Conference/ Meeting/ Multipurpose", value: 0.60},
+    {spaceType: "Convention center - Exhibit space", value: 0.60},
+    {spaceType: "Corridor/ Transition", value: 0.60},
+    {spaceType: "Dining area", value: 0.60},
+    {spaceType: "Electrical/ Mechanical", value: 0.60},
+    {spaceType: "Food preparation", value: 0.60},
+    {spaceType: "Lobby", value: 0.60},
+    {spaceType: "Restrooms", value: 0.60},
+    {spaceType: "Stairs - Active", value: 0.60}
 ];
-//Envelope Washington Code
+*/
 
-const envWashingtonCode = [
-    {envelope: "Wall", code: "U-0.055 (R-18)"},
-    {envelope: "Roof", code: "U-0.027 (R-37)"},
-    {envelope: "Window U-Value", code: "U-0.3"},
-    {envelope: "Window SHGC", code: "SHGC-0.4"},
-    {envelope: "Window to Wall Ratio", code: "30% all sides"},
-    {envelope: "Shading Devices", code: "None"}
+//Envelope Washington value
+
+const envWashingtonvalue = [
+    {label: "Wall", value: "U-0.055 (R-18)", alt:"U-0.053 (R-19)" },
+    {label: "Roof", value: "U-0.027 (R-37)", alt:"U-0.034 (R-30)" },
+    {label: "Window U-Value", value: "U-0.3", alt:"U-0.2" },
+    {label: "Window SHGC", value: "SHGC-0.4", alt:"SHGC-0.26" },
+    {label: "Window to Wall Ratio", value: "30% all sides", alt:"~100% on 3/4 floor \n 75% overall" },
+    {label: "Shading Devices", value: "None", alt:"Vertical fins on the NW and SW facades \n Mature trees on the NE and SE sides" }
 ];
+
+/*
 //Envelope Proposed Design
 
 const envProposedDesign = [
-    {envelope: "Wall", code: "U-0.053 (R-19)"},
-    {envelope: "Roof", code: "U-0.034 (R-30)"},
-    {envelope: "Window U-Value", code: "U-0.2"},
-    {envelope: "Window SHGC", code: "SHGC-0.26"},
-    {envelope: "Window to Wall Ratio", code: "~100% on 3/4 floor \n 75% overall"},
-    {envelope: "Shading Devices", code: "Vertical fins on the NW and SW facades \n Mature trees on the NE and SE sides"}
+    {envelope: "Wall", value: "U-0.053 (R-19)"},
+    {envelope: "Roof", value: "U-0.034 (R-30)"},
+    {envelope: "Window U-Value", value: "U-0.2"},
+    {envelope: "Window SHGC", value: "SHGC-0.26"},
+    {envelope: "Window to Wall Ratio", value: "~100% on 3/4 floor \n 75% overall"},
+    {envelope: "Shading Devices", value: "Vertical fins on the NW and SW facades \n Mature trees on the NE and SE sides"}
 ];
-//HVAC System	Washington Code
+*/
 
-const HVACWashCode = [
-    {HVACSystem: "Ventilation System", code: "DOAS"},
-    {HVACSystem: "Zone System", code: "Fan Coils"},
-    {HVACSystem: "Zone Fan Control", code: "Constant"},
-    {HVACSystem: "Heat recovery %", code: "50%"},
-    {HVACSystem: "Cooling COP", code: "6.0"},
-    {HVACSystem: "Heating COP", code: "0.9"}
+//HVAC System	Washington value
+
+const HVACWashvalue = [
+    {label: "Ventilation System", value: "DOAS", alt: "DOAS" },
+    {label: "Zone System", value: "Fan Coils", alt: "Radiant" },
+    {label: "Zone Fan Control", value: "Constant", alt: "N/A" },
+    {label: "Heat recovery %", value: "50%", alt: "75%" },
+    {label: "Cooling COP", value: "6.0", alt: "6.0" },
+    {label: "Heating COP", value: "0.9", alt: "0.9" }
 ];
+
+/*
 //HVAC System	Proposed Design
-
 const HVACProposedDesign = [
-    {HVACSystem: "Ventilation System", code: "DOAS"},
-    {HVACSystem: "Zone System", code: "Radiant"},
-    {HVACSystem: "Zone Fan Control", code: "N/A"},
-    {HVACSystem: "Heat recovery %", code: "75%"},
-    {HVACSystem: "Cooling COP", code: "6.0"},
-    {HVACSystem: "Heating COP", code: "0.9"}
+    {HVACSystem: "Ventilation System", value: "DOAS"},
+    {HVACSystem: "Zone System", value: "Radiant"},
+    {HVACSystem: "Zone Fan Control", value: "N/A"},
+    {HVACSystem: "Heat recovery %", value: "75%"},
+    {HVACSystem: "Cooling COP", value: "6.0"},
+    {HVACSystem: "Heating COP", value: "0.9"}
 ];
 
+*/
 
-
-const table = d3.select(".table").append('table');
+const table = d3.select(".spaceType-table").append('table');
 const thead = table.append('thead');
 const tbody = table.append('tbody');
 
 thead.selectAll('th')
-  .data(columns)
+  .data(columns[0])
   .enter()
   .append('th')
-  .attr('class', function(d){ if (d === 'Space Type'){ return "spaceType"}})
+  .attr('class', function(d){ if (d[0]){ return "spaceType"}})
   .text(function (d) {return d;})
 
-  /*
+
 //you are now adding all data to all tr
 const row = tbody.selectAll('tr')
   .data(WSECDesign)
@@ -95,10 +105,11 @@ const cells = row.selectAll('td')
   })
   .enter()
   .append('td')
-  .text(function(d) { return d.column})
-*/
+  .attr( 'class', function (d) { if(d.column === "label"){return 'spaceType' }})
+  .text(function(d) { return d.value})
 
 
+/*
 //you are now adding all data to all tr
 table.append('tbody')
   .selectAll('tr')
@@ -115,8 +126,6 @@ table.append('tbody')
   .append('td')
   .attr( 'class', function (d) { if(d.column === "spaceType"){return 'spaceType' }})
   .text(function(d) { return d.value})
+*/
 
 
-
-
-//d3.select('.table').append('table').append('tr').selectAll('td').data
